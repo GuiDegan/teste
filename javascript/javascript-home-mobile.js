795556cf88fdfee099ef8dda1410104f2a6706d8
Guilherme Degan <60679103+GuiDegan@users.noreply.github.com>
2020-09-23 19:17:36 -0300

    Add files via upload

---
+++ javascript/javascript-home-mobile.js
@@ -0,0 +1,11 @@
+const navbarButtonToggle = () => {
+  var navbarToggler = document.querySelector(".navbar-toggler");
+  var iconHamburger = document.querySelector(".navbar-toggler-icon");
+  var iconClose = document.querySelector(".navbar-toggler-icon-close");
+
+  navbarToggler.addEventListener("click", () => {
+    iconHamburger.classList.toggle("d-none");
+    iconClose.classList.toggle("d-none");
+  });
+};
+navbarButtonToggle();
